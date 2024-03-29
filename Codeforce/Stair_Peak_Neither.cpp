ec6ec97086183dd0b1e6fb2ce1ae3b4ea118f31c
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2024-03-29 10:36:34 +0600

    1950A

---
+++ Codeforce/Stair_Peak_Neither.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int tt;	cin >> tt;
+	while(tt--){
+		int a,b,c;	cin >> a >> b >> c;
+		if(a<b && b<c) cout << "STAIR\n";
+		else if(a<b && b>c)	cout << "PEAK\n";
+		else cout << "NONE\n";
+	}
+}
